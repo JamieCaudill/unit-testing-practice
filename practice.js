95d299582557d51835aa981ec299eaee7d8c3313
Jamie Caudill <j.caudill7177@gmail.com>
2023-03-22 14:46:50 -0700

    Write findRemainder and startGPS functions

--- practice.js
+++ practice.js
@@ -1,10 +1,18 @@
-function findRemainder() {
+function findRemainder(i, j) {
+  return i % j;
+};
 
-}
+function startGPS(currentLocation, newLocation) {
+  if (currentLocation ===  newLocation) {
+    return (`You are already at the ${currentLocation}!  Would you like to go somewhere else?`);
+  } else {
+  return(`Taking you from ${currentLocation} to ${newLocation}!  Getting directions now!`);
+  }
+};
 
 module.exports = {
   findRemainder,
-  // startGPS,
+  startGPS,
   // createUser,
   // makePizza
 }
